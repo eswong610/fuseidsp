47a40314dddd859774e3cfff7e2345d0f332712b
Emmy <ewong96@live.ca>
2020-04-28 19:06:56 -0700

    socket can chat now!!!

--- src/server/routes/socket.js
+++ src/server/routes/socket.js
@@ -5,12 +5,42 @@ module.exports = function (io) {
     
     
     router.get('/message', (req,res)=>{
-        let socket = req.app.get('socketio');
-        socket.on('connection', (socket) => {
-            console.log('a user connected');
-          });
+        let io = req.app.get('socketio');
+        if(io.sockets._events == undefined) {
+            io.on('connection', socket => {
+                // console.log('%s sockets connected', io.engine.clientsCount);
+                console.log(socket.id + ' is connected');
+
+                socket.on('chat message', (msg) => {
+                    io.emit('chat message', msg)
+                    console.log(`${socket.id} sent ${msg}`);
+                });
+
+                socket.on('disconnect',()=>{
+                    socket.removeAllListeners();
+                    console.log('user disconnected')
+                })
+            });
+          }
+        // io.on('connection', (socket) => {
+            
+        //     console.log(' %s sockets connected', io.engine.clientsCount);
+        //     console.log(socket.id + ' is connected')
+            
+        //     socket.on('chat message', (msg) => {
+        //         io.emit('chat message: ' + msg)
+        //         console.log('message: ' + msg);
+        //     });
+
+        //     socket.on('disconnect',()=>{
+        //         socket.removeAllListeners();
+        //         console.log('user disconnected')
+        //     })
+        // });
+
         res.render('messaging');
-    })
+    });
+
     
 
     return router
