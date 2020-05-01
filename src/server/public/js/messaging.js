98bd3af143db4d5088cd661f8d68e5896223e9a5
Emmy <ewong96@live.ca>
2020-05-01 07:37:28 -0700

    names in chat

--- src/server/public/js/messaging.js
+++ src/server/public/js/messaging.js
@@ -1,35 +1,40 @@
 $(function () {
         
-    //var socket = io();
+    //MESSAGES 
     var socket = io({transports: ['websocket'], upgrade: false});
+
+    const name = prompt('Enter your name')
+    $('#messages').append($('<li>').text('You joined'))
+    socket.emit('new-user', name)
+
+    // $('#messages').append($('<li>').text(''))
+
+
+    socket.on('chat message', function(data){
+
+        let fullmsg = `${data.name}: ${data.message}`
+        $('#messages').append($('<li>').text(fullmsg));
+    });
+
     $('.msg-form').submit(function(e) {
       e.preventDefault(); // prevents page reloading
-      console.log($('#m').val());
-      socket.emit('chat message', $('#m').val());
+    //   console.log($('#m').val());
+      const message = ($('#m').val());
+      let mymessage = `You: ${message}`
+      $('#messages').append($('<li>').text(mymessage))
+      socket.emit('send chat message', message);
       $('#m').val('');
-      return false;
+      
     });
 
+    socket.on('user-connected',(name)=>{
+        $('#messages').append($('<li>').text(`${name} connected`))
+    })
 
-    socket.on('chat message', function(msg){
-        
-        let fullmsg = `${socket.id}: ${msg}`
-    $('#messages').append($('<li>').text(fullmsg));
-    });
+    //ROOMS (DONT TOUCH )
 
     socket.on('room-created', function(room) {
         $('.room-container').append(`<div>${room}</div><a href='/socket/${room}'>Join</a>`)
-        
-
-        // const roomElement = document.createElement('div');
-        // roomElement.innerText = room
-        // // const roomLink = document.createElement('a');
-        // // roomLink.href=`/sockets/${room}`;
-        // // roomLink.innerText('Join')
-        // const roomContainer = document.getElementsByClassName('room-container');
-        // roomContainer.append('roomElement');
-        // roomContainer.append('roomLink');
-
         console.log('from socketon room created' + room )
 
     })
--- src/server/routes/socket.js
+++ src/server/routes/socket.js
@@ -6,21 +6,33 @@ module.exports = function () {
     //IT WORKS DNOT TOUCH THIS
     //Socket.io for live chatting.
     router.get('/message', (req,res)=>{
+
         let io = req.app.get('socketio');
+        const users = {};
         
         if(io.sockets._events == undefined) {
             io.on('connection', socket => {
                 // console.log('%s sockets connected', io.engine.clientsCount);
                 console.log(socket.id + ' is connected');
 
-                socket.on('chat message', (msg) => {
-                    io.emit('chat message', msg)
-                    console.log(`${socket.id} sent ${msg}`);
-                });
+                // socket.on('chat message', (msg) => {
+                //     io.emit('chat message', {msg:msg, name: users[socket.id]})
+                //     console.log(`${socket.id} sent ${msg}`);
+                // });
 
                 socket.on('disconnect',()=>{
                     socket.removeAllListeners();
                     console.log('user disconnected')
+                })
+
+                socket.on('send chat message', message=>{
+                    socket.broadcast.emit('chat message', {message:message, name: users[socket.id]})
+                    console.log(`${socket.id} sent ${message}`);
+                })
+
+                socket.on('new-user',(name)=>{
+                    users[socket.id]= name;
+                    socket.broadcast.emit('user-connected', name)
                 })
             });
           }
