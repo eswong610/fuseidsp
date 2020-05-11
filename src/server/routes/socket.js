44be8d807d0a516d3e4b48759ef286fd971e56b3
Emmy <ewong96@live.ca>
2020-05-11 03:22:12 -0700

    rly fucked up msgs

--- src/server/routes/socket.js
+++ src/server/routes/socket.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const router = express.Router();
-const Prompt = require('../models/Prompt').Prompt
+const Prompt = require('../models/Prompt').Prompt;
+const { ensureAuthenticated } = require('./public-controller');
+const User = require('../models/User').User;
+const Message = require('../models/Message').Message;
 
 
 module.exports = function () {
@@ -12,88 +15,81 @@ module.exports = function () {
     
     //IT WORKS DNOT TOUCH THIS
     //Socket.io for live chatting.
-    router.get('/message', (req,res)=>{
-
-        let io = req.app.get('socketio');
+    router.get('/message', ensureAuthenticated, (req,res)=>{
+        console.log('from get /message');
+        
         const users = {};
+
+        User.findOne({username: req.user.username})
+                .then((data)=>{
+                    console.log(data['likedpeople'])
+                    for (i of data['likedpeople']) {
+                        rooms[i] = i
+                    }
+                })
+                .catch((err)=>console.log(err));
         
-        if(io.sockets._events == undefined) {
-            io.on('connection', socket => {
-                // console.log('%s sockets connected', io.engine.clientsCount);
-                console.log(socket.id + ' is connected');
-
-                // socket.on('chat message', (msg) => {
-                //     io.emit('chat message', {msg:msg, name: users[socket.id]})
-                //     console.log(`${socket.id} sent ${msg}`);
-                // });
-
-                socket.on('disconnect',()=>{
-                    socket.removeAllListeners();
-                    console.log('user disconnected')
-                })
-
-                socket.on('send chat message', message=>{
-                    socket.broadcast.emit('chat message', {message:message, name: users[socket.id]})
-                    console.log(`${socket.id} sent ${message}`);
-                })
-
-                socket.on('new-user',(name)=>{
-                    users[socket.id]= name;
-                    socket.broadcast.emit('user-connected', name)
-                })
-            });
-          }
-
         
-        Prompt.findRandom({},{},{limit:3}, (err,data)=>{
+          Prompt.findRandom({},{},{limit:3}, (err,data)=>{
             if (err) throw err;
-            console.log(data);
+            //console.log(data);
             //data[i]['prompt]
-            res.render('messaging')
-        })
+            res.render('messaging',{
+                prompts: data,
+                
+            })
+          })
         
-
-
-        // res.render('messaging')//, {
-           //not sure about this....... 
-            //javascript: "../public/js/messaging.js"
-        //});
     });
 
+    
 
-    //test rooms 
-    rooms = {trialroom : 'hello'}
 
-    router.get('/rooms', (req,res)=>{
-        res.render('rooms', {rooms:rooms})
-    })
+    //Chat rooms 
+    rooms = {}
 
-    router.post('/rooms', (req,res)=>{
-        if (rooms[req.body.formroom] != null) {
-            return res.redirect('/rooms')
-        }
-        console.log(req.body)
-        rooms[req.body.formroom] = { users: {} }
+    // router.get('/rooms', (req,res)=>{
+       
+    //     res.render('rooms', {rooms:rooms})
+    // })
 
-        //Sends message that new room is created
+    // router.post('/rooms', (req,res)=>{
+    //     if (rooms[req.body.formroom] != null) {
+    //         return res.redirect('/rooms')
+    //     }
+    //     console.log(req.body)
+    //     rooms[req.body.formroom] = { users: {} }
+
+    //     //Sends message that new room is created
         
-        res.redirect(req.body.formroom);
+    //     res.redirect(req.body.formroom);
 
-        let io = req.app.get('socketio');
-        io.emit('room-created', req.body.formroom)
+    //     let io = req.app.get('socketio');
+    //     io.emit('room-created', req.body.formroom)
 
-    })
+    // })
 
-    router.get('/:room', (req,res)=>{
+
+    router.get('/:room', ensureAuthenticated, (req,res)=>{
+        console.log(rooms[req.params.rooms])
         if (rooms[req.params.room] == null) {
-            res.redirect('rooms')
+            User.findOne({username: req.user.username})
+                .then((data)=>{
+                    console.log(data['likedpeople'])
+                    for (i of data['likedpeople']) {
+                        rooms[i] = i
+                    }
+                })
+                .catch((err)=>console.log(err));
         }
-
+        
         let io = req.app.get('socketio');
         const users = {};
         
         if(io.sockets._events == undefined) {
+            console.log(req.params.rooms)
             io.on('connection', socket => {
+                
                 // console.log('%s sockets connected', io.engine.clientsCount);
                 console.log(socket.id + ' is connected');
 
@@ -109,6 +105,18 @@ module.exports = function () {
                 socket.on('send chat message', message=>{
                     socket.broadcast.emit('chat message', {message:message, name: users[socket.id]})
                     console.log(`${socket.id} sent ${message}`);
+                    console.log(rooms[req.params.room]); 
+                    const newMessage = new Message({
+                        text: message,
+                        sender: req.user.username,
+                        receiver: rooms[req.params.room] 
+                    })
+                    newMessage.save()
+                    .then(data=>{
+                        console.log(`${data.sender} sent ${data.text} to ${data.receiver}`)
+                    })
+                    .catch(err=>console.log(err))
+
                 })
 
                 socket.on('new-user',(name)=>{
--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -1,7 +1,9 @@
 const express= require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('./public-controller');
-const User = require('../models/User').User
+const User = require('../models/User').User;
+const Prompt = require('../models/Prompt').Prompt;
+const Message = require('../models/Message').Message;
 
 
 module.exports = function () {
@@ -52,6 +54,41 @@ module.exports = function () {
         // res.send(req.params.username + 'this is the profile you\'re looking for')
     })
 
+    const chattingTo = {}
+
+    router.get('/users/message/:username', (req,res)=>{
+
+        chattingTo['user'] = req.params.username
+
+        Prompt.findRandom({},{},{limit:3}, (err,data)=>{
+            if (err) throw err;
+            //console.log(data);
+            //data[i]['prompt]
+            res.render('staticmsg',{
+                prompts: data,
+                chattingTo: req.params.username
+                
+            })
+          })
+
+    })
+
+    router.post('/staticmessage', ensureAuthenticated, (req,res)=>{
+        
+        const newMessage = new Message({
+            text: message['message'],
+            sender: req.user.username,
+            receiver: chattingTo['user']
+        })
+        newMessage.save()
+        .then(data=>{
+            console.log(`${data.sender} sent ${data.text} to ${data.receiver}`)
+        })
+        .catch(err=>console.log(err));
+        res.redirect('/users/message/'+chattingTo['user'])
+    })
+    
+
 
     router.post('/bio-update', (req,res)=>{
         User.updateOne(
@@ -73,9 +110,11 @@ module.exports = function () {
         if (likedByUser) {
             likedByUser = likedByUser.substring(2);
         // console.log(likedByUser.substring(2))
+        
+
         User.updateOne(
             {username:req.user.username},
-            {$push: {likedpeople: likedByUser}}
+            {$addToSet: {likedpeople: likedByUser}}
         )
         .then((data)=>{
             console.log(`updated ${data}`)
